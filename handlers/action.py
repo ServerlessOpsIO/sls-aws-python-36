4bdf945a9eeea2d968105b377e68de1d3bfb5d27
Tom McLaughlin <tom@serverlessops.io>
2018-04-09 20:50:43 -0400

    Cleanup this function so it doesn't trip checkers off the bat

--- handlers/action.py
+++ handlers/action.py
@@ -5,12 +5,13 @@ import json
 import os
 
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
-logging.root.setLevel(logging.getLevelName(log_level))
+logging.root.setLevel(logging.getLevelName(log_level))  # type: ignore
 _logger = logging.getLogger(__name__)
+
 
 def handler(event, context):
     '''Function entry'''
 
     resp = {}
-    return json.dumps(resp)
+    return resp
 
