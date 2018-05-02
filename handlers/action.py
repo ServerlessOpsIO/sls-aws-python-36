d216875e4df457987c02dc49b59296614532c32c
Tom McLaughlin <tom@serverlessops.io>
2018-05-02 16:37:28 -0400

    Add them debugging statements

--- handlers/action.py
+++ handlers/action.py
@@ -11,7 +11,9 @@ _logger = logging.getLogger(__name__)
 
 def handler(event, context):
     '''Function entry'''
+    _logger.debug('Event received: {}'.format(json.dumps(event)))
 
     resp = {}
+    _logger.debug('Response: {}'.format(json.dumps(resp)))
     return resp
 
