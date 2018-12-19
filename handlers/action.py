289e251efcafa0ab8e27420251c17dc769ffcecb
Tom McLaughlin <tom@serverlessops.io>
2018-12-19 14:00:36 -0500

    Default to info for logging.

--- handlers/action.py
+++ handlers/action.py
@@ -11,9 +11,9 @@ _logger = logging.getLogger(__name__)
 
 def handler(event, context):
     '''Function entry'''
-    _logger.debug('Event: {}'.format(json.dumps(event)))
+    _logger.info('Event: {}'.format(json.dumps(event)))
 
     resp = {}
-    _logger.debug('Response: {}'.format(json.dumps(resp)))
+    _logger.info('Response: {}'.format(json.dumps(resp)))
     return resp
 
